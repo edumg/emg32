1e4135778643b56f264ae7ba635ae9301e13d435
Jean Rigotti <gmgotti@protonmail.com>
2018-02-17 09:43:57 +0100

    Integrate redux

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import srcReducer from './src/reducers';
 
 import App from './src/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+let store = createStore(
+    srcReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    ),
+    document.getElementById('app'));
