7ed44e1dfbe6762d52a24a272ec336459ff19efa
Jean Rigotti <gmgotti@protonmail.com>
2018-02-18 05:49:24 +0100

    Add postcss to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <header>
+            <header className="header">
                 <h1>Rigotti</h1>
                 <div>Select language:</div>
             </header>
