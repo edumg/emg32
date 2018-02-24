362a0915bcd39e9a9b0c00f8eeba8164d2596799
Jean Rigotti <gmgotti@protonmail.com>
2018-02-24 16:48:48 +0100

    Start value in the redux tree

---
+++ src/reducers/meta.js
@@ -0,0 +1,12 @@
+const initialState = {
+    language: 'nl'
+};
+
+const meta = (state = initialState, action) => {
+    switch (action.type) {
+    default:
+        return state;
+    }
+};
+
+export default meta;
