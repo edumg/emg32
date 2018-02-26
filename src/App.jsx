887de33a6be150997ad62220f5e56ad50f5d7e9e
Jean Rigotti <gmgotti@protonmail.com>
2018-02-26 16:31:14 +0100

    Better presentation

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,38 @@ class App extends Component {
     render() {
         return (
             <main>
-                <h1>Rigotti</h1>
+                <h1>Hello World!</h1>
                 <p>
-                    My name is Jean Rigotti and I am full stack developer based
-                    in Amsterdam.
+                    My name is Jean Rigotti and I am Brazilian Full Stack
+                    Developer based and working in Amsterdam.
                 </p>
                 <p>
-                    You see more about in those links:
+                    I am not actively looking for a job, but if you're looking
+                    to fill a position which my skills match or can be improved;
+                    we can sit for a coffee.
+                </p>
+                <p>
+                    Be aware that I don't have a linkedin account anymore, as
+                    the means of growing an attractive profile is completely
+                    unrelated with one's ability to be a good developer, but
+                    your ability to spend time on it.
+                </p>
+                <p>
+                    If you wish to see have a better overview of my development
+                    skills:
                     <ul>
                         <li>
-                            <a href="#">Github</a>
+                            <a href="https://github.com/JRigotti">Github</a>
                         </li>
                         <li>
-                            <a href="#">Stack Overflow</a>
+                            <a href="https://stackoverflow.com/users/1657165/rigotti">
+                                Stack Overflow
+                            </a>
                         </li>
                         <li>
-                            <a href="#">Instagram</a>
-                        </li>
-                        <li>
-                            <a href="#">Blog</a>
+                            <a href="mailto:gmgotti@protonmail.com">
+                                gmgotti@protonmail.com
+                            </a>
                         </li>
                     </ul>
                 </p>
